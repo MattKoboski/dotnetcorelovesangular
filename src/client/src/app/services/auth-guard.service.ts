9439d16c92e0742c5d9cf83ecefe6467e8c566e9
Marcin Miazga <marcin@miazga.pl>
2018-08-08 19:45:14 +0200

    changed structure of auth

--- src/client/src/app/services/auth-guard.service.ts
+++ src/client/src/app/services/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router, ActivatedRouteSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
+import { AUTH_CONFIG } from './auth0-variables';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -11,7 +12,7 @@ export class AuthGuard implements CanActivate {
       if (this.auth.isInRoles(route.data.requiredRoles))
         return true;
       
-      this.router.navigateByUrl('https://tim-lab.eu.auth0.com/login?client=3t5oWTvM24JLOSiu52aWnldLisop9Ngo');
+      this.router.navigateByUrl(`https://tim-lab.eu.auth0.com/login?client=${AUTH_CONFIG.clientID}`);
       return false;
     }
     
